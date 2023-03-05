a59120cc14fd61d43b8572725934ef9c0672b529
James Hodgkinson <james@terminaloutcomes.com>
2023-03-06 09:53:34 +1000

    adding find-curl-update-path

---
+++ find-curl-update-path.sh
@@ -0,0 +1,13 @@
+#shellcheck shell=bash
+# source this in your shell and you'll get the homebrew-installed curl first
+
+CURL_DIR="$(find /opt/homebrew/Cellar/curl -maxdepth 1 -mindepth 1 -type d | head -n1)"
+
+if [ -z "${CURL_DIR}" ]; then
+    echo "Couldn't find curl"
+    exit 1
+else
+    echo "Found curl dir: ${CURL_DIR}"
+fi
+
+PATH="${CURL_DIR}/bin:$PATH"
