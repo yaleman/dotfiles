86403d0de0b37d34b06af1f2d20af2af3acd795e
James Hodgkinson <james@terminaloutcomes.com>
2023-05-20 08:47:46 +1000

    starting to work on chrome-cli support for howmanytabs

--- howmanytabs.sh
+++ howmanytabs.sh
@@ -2,11 +2,21 @@
 
 set -e
 
-function f () {
+function safariTabs () {
     #shellcheck disable=SC2069
     osascript howmanytabsraw 2>&1 >/dev/null
 }
 
 
 #shellcheck disable=SC2069
-f 2>&1 >/dev/null
+safariTabs 2>&1 >/dev/null
+
+if [ "$(ps aux | rg -c 'Microsoft Edge\.app')" -gt 1 ]; then
+    # TODO: finish using chrome-cli to do chrome/edge tabs
+    export CHROME_BUNDLE_IDENTIFIER="com.microsoft.edgemac"
+
+    # TABS=$(chrome-cli list tabs | wc -l)
+    # WINDOWS=$(chrome-cli list windows | wc -l)
+
+    # curl -sf https://$()
+fi
