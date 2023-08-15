8022648ae5e1ad2b9b59b41aa806880228afb545
James Hodgkinson <james@terminaloutcomes.com>
2023-08-16 08:51:24 +1000

    looping screen thing

---
+++ loopscreen.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# This loops the screen cammand for pesky serial devices which keep disappearing
+
+if [ -z "$1" ]; then
+    echo "Specify a target device"
+    exit 1
+fi
+
+while true; do
+    if [ "$(find dirname "${1}" -name "$(basename "${1}")" | wc -l)" -eq 1 ]; then
+        screen "${1}"
+    else
+        sleep 1
+    fi
+done
