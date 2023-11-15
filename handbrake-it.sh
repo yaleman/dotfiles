2d6cc49403dfc707efc747923fca41c03444727b
James Hodgkinson <james@terminaloutcomes.com>
2023-11-15 21:32:14 +1000

    fixing handbrake-it

--- handbrake-it.sh
+++ handbrake-it.sh
@@ -4,29 +4,32 @@
 
 INPUT_FILENAME="$1"
 #shellcheck disable=SC2001
-OUTPUT="$(echo "$1" | sed -e s/mp4/mkv/)"
+OUTPUT_FILENAME="$(echo "$1" | sed -E 's/(m4v|mp4)$/mkv/' )"
 
-if [ ! -f "${OUTPUT}" ]; then
-    echo "Writing ${OUTPUT}"
+if [ ! -f "${OUTPUT_FILENAME}" ]; then
+    echo "Writing ${OUTPUT_FILENAME}"
     HandBrakeCLI --encoder vt_h265_10bit  \
         --vfr \
         --format av_mkv \
         -i "${INPUT_FILENAME}" \
-        -o "${OUTPUT}"
+        -o "${OUTPUT_FILENAME}"
 
-    # ensure $OUTPUT exists and is non-zero in size
-    if [ ! -s "${OUTPUT}" ]; then
-        echo "ERROR: ${OUTPUT} is zero size"
+    # ensure $OUTPUT_FILENAME exists and is non-zero in size
+    if [ ! -s "${OUTPUT_FILENAME}" ]; then
+        echo "ERROR: ${OUTPUT_FILENAME} is zero size"
         exit 1
     fi
 
-    # compare $INPUT_FILENAME and $OUTPUT and delete the larger one
+    # compare $INPUT_FILENAME and $OUTPUT_FILENAME and delete the larger one
     INPUT_SIZE=$(stat -f%z "${INPUT_FILENAME}")
-    OUTPUT_SIZE=$(stat -f%z "${OUTPUT}")
-    if [ "${INPUT_SIZE}" -gt "${OUTPUT_SIZE}" ]; then
+    OUTPUT_FILENAME_SIZE=$(stat -f%z "${OUTPUT_FILENAME}")
+    if [ "${INPUT_SIZE}" -gt "${OUTPUT_FILENAME_SIZE}" ]; then
         echo "Deleting ${INPUT_FILENAME}"
         rm "${INPUT_FILENAME}"
+    else
+        echo "New file is larger ${OUTPUT_FILENAME_SIZE} > ${INPUT_SIZE}} - deleting ${OUTPUT_FILENAME}"
+        rm "${OUTPUT_FILENAME}"
     fi
 else
-    echo "Skipping ${INPUT_FILENAME} - ${OUTPUT} already exists"
+    echo "Skipping ${INPUT_FILENAME} - ${OUTPUT_FILENAME} already exists"
 fi
