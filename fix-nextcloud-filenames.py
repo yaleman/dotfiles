5dc25115bcc5a44a4075f5d9e3c03bf8ef53f47e
James Hodgkinson <james@terminaloutcomes.com>
2024-10-08 10:36:16 +1000

    more fix-nextcloud-things

--- fix-nextcloud-filenames.py
+++ fix-nextcloud-filenames.py
@@ -1,23 +1,32 @@
 #!python3
+from functools import lru_cache
 import os
 from pathlib import Path
 import sys
-from typing import Optional
+from typing import Optional, Set
 
 import click
 
-BAD_CHARS = [" — ", "{", "}", "!", "@", "#", "$", "%", "^", "&", "*", "=", "|", "\\", "/", "?", "<", ">", ":", ";", "`"]
+# the key is what it ends up as, the list of values is the things that get replaced
+BAD_CHAR_MAPS = {
+    "-" : [ "|" ]
+}
 
+BAD_CHARS = {" — ", "{", "}", "!", "@", "#", "$", "%", "^", "&", "*", "=",  "\\", "/", "?", "<", ">", ":", ";", "`", "“","”"}
+
+@lru_cache()
+def all_bad_chars()-> Set[str]:
+    all_chars = set()
+    for key, values in BAD_CHAR_MAPS.items():
+        all_chars.add(key)
+        all_chars.update(values)
+    return set.union(all_chars, BAD_CHARS)
 
 @click.command()
-@click.option("--filepath", type=click.Path(exists=True))
-def fix_nextcloud_filenames(filepath: Optional[str]=None) -> None:
+@click.argument("filepath", type=click.Path(exists=True), envvar='FIX_NEXTCLOUD_DEFAULT_PATH')
+@click.option("--debug", is_flag=True)
+def fix_nextcloud_filenames(filepath: str, debug: Optional[bool]=False) -> None:
 
-    if filepath is None:
-        filepath = os.getenv("FIX_NEXTCLOUD_DEFAULT_PATH")
-    if filepath is None:
-        print("Please provide a file path or set the env var FIX_NEXTCLOUD_DEFAULT_PATH.")
-        sys.exit(1)
     path = Path(filepath)
 
     if not path.exists():
@@ -26,18 +35,28 @@ def fix_nextcloud_filenames(filepath: Optional[str]=None) -> None:
 
     print(f"Checking {filepath}", file=sys.stderr)
 
+
     for filename in path.iterdir():
         if filename.is_file():
-            if any(char in filename.name for char in BAD_CHARS):
-                new_name = filename.name
+            if any(char in filename.name for char in all_bad_chars()):
+                new_name = str(filename.name)
                 for char in BAD_CHARS:
                     new_name = new_name.replace(char, " ")
                 while "  " in new_name:
                     new_name = new_name.replace("  ", " ")
+
+                for (key, values) in BAD_CHAR_MAPS.items():
+                    for value in values:
+                        new_name = new_name.replace(value, key)
+
                 new_name = new_name.strip()
+
                 new_filepath = filename.parent / new_name
-                print(f"Renaming '{filename}' to '{new_filepath}'")
-                filename.rename(new_filepath)
+
+
+                if new_name != filename.name:
+                    print(f"Renaming '{filename}' to '{new_filepath}'")
+                    filename.rename(new_filepath)
 
 
 if __name__ == "__main__":
