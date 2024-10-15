2ad0d116de9be5161cce0265ca7e0fec1cc208f2
James Hodgkinson <james@terminaloutcomes.com>
2024-10-15 12:41:44 +1000

    trying to fix parents as well

--- find_and_fix_uid_gid.py
+++ find_and_fix_uid_gid.py
@@ -5,7 +5,7 @@
 
 from pathlib import Path
 import sys
-from typing import Optional
+from typing import Optional, Set
 import click
 
 import os
@@ -15,6 +15,37 @@ IGNORE_ROOTS = (
     "/sys",
     "/dev",
 )
+
+
+def do_thing(
+    file_path: Path, uid: Optional[int], gid: Optional[int], new_uid: Optional[int], new_gid: Optional[int], debug: bool
+) -> None:
+    if debug:
+        print(f"Checking {file_path}")
+
+    try:
+        file_stat = os.stat(file_path)
+        if (uid is not None and file_stat.st_uid == uid) or (gid is not None and file_stat.st_gid == gid):
+            print(f"Found matching file='{file_path}' UID={file_stat.st_uid}, GID={file_stat.st_gid}")
+            if new_uid is not None and file_stat.st_uid != new_uid:
+                new_file_uid = new_uid
+            else:
+                new_file_uid = file_stat.st_uid
+            if new_gid is not None and file_stat.st_gid != new_gid:
+                new_file_gid = new_gid
+            else:
+                new_file_gid = file_stat.st_gid
+            os.chown(file_path, new_file_uid, new_file_gid)
+        else:
+            if debug:
+                print(f"Skipping file='{file_path}' UID={file_stat.st_uid}, GID={file_stat.st_gid}")
+    except PermissionError as error:
+        print(f"Permission denied: file_path={file_path} error={error}")
+    except FileNotFoundError:
+        if debug:
+            print(f"File not found: file_path={file_path}")
+    except Exception as error:
+        print(f"Error processing file_path={file_path}: error={error}")
 
 
 @click.command()
@@ -36,42 +67,26 @@ def find_files_by_uid_gid(
         print("No UID or GID provided, exiting")
         sys.exit(1)
 
+    done_paths: Set[Path] = set()
+
     for root, _dirs, files in os.walk(start_path):
         if root in IGNORE_ROOTS or root.startswith(tuple(IGNORE_ROOTS)):
             if debug:
                 print(f"Skipping root={root}")
             continue
         for file in files:
-            file_path = os.path.join(root, file)
-            file_path_path = Path(file_path)
-            if not file_path_path.is_file() and not file_path_path.is_dir():
+            file_path = Path(os.path.join(root, file))
+            if not file_path.is_file() and not file_path.is_dir():
                 if debug:
                     print(f"Skipping as not file/dir: {file_path}", file=sys.stderr)
                 continue
+            if file_path not in done_paths:
+                do_thing(file_path, uid, gid, new_uid, new_gid, debug)
+                done_paths.add(file_path)
 
-            try:
-                file_stat = os.stat(file_path)
-                if (uid is not None and file_stat.st_uid == uid) or (gid is not None and file_stat.st_gid == gid):
-                    print(f"Found matching file='{file_path}' UID={file_stat.st_uid}, GID={file_stat.st_gid}")
-                    if new_uid is not None and file_stat.st_uid != new_uid:
-                        new_file_uid = new_uid
-                    else:
-                        new_file_uid = file_stat.st_uid
-                    if new_gid is not None and file_stat.st_gid != new_gid:
-                        new_file_gid = new_gid
-                    else:
-                        new_file_gid = file_stat.st_gid
-                    os.chown(file_path, new_file_uid, new_file_gid)
-                else:
-                    if debug:
-                        print(f"Skipping file='{file_path}' UID={file_stat.st_uid}, GID={file_stat.st_gid}")
-            except PermissionError as error:
-                print(f"Permission denied: file_path={file_path} error={error}")
-            except FileNotFoundError:
-                if debug:
-                    print(f"File not found: file_path={file_path}")
-            except Exception as error:
-                print(f"Error processing file_path={file_path}: error={error}")
+            if file_path.parent not in done_paths:
+                do_thing(file_path.parent.resolve(), uid, gid, new_uid, new_gid, debug)
+                done_paths.add(file_path.parent)
 
 
 if __name__ == "__main__":
